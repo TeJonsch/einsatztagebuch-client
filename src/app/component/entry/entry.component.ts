11f5a12c4f68379f32e5213e7416652b3c1eceef
Jonas Hippler <jonas.hippler@atruvia.de>
2025-06-19 22:03:12 +0200

    use material

--- src/app/component/entry/entry.component.ts
+++ src/app/component/entry/entry.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { DiaryEntryDto } from '../../model/diary-entry.model';
+import { MatCard, MatCardContent, MatCardFooter, MatCardHeader } from '@angular/material/card';
 
 @Component({
     selector: 'app-entry',
-    imports: [],
+    imports: [MatCard, MatCardContent, MatCardFooter, MatCardHeader],
     templateUrl: './entry.component.html',
     styleUrl: './entry.component.scss',
 })
